b68ad611bbb81e462ef03557071f84210659826c
Whien <sal95610@gmail.com>
2017-10-06 16:52:38 +0800

    add test

--- src/__test__/state-provider.spec.js
+++ src/__test__/state-provider.spec.js
@@ -3,3 +3,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+describe('<StateProvider />', () => {
+  it('render StateProvider component', () => {});
+});
